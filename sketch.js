db4b3f71d576269eb059cb54775cb80968333739
bhats478 <bhats478@newschool.edu>
2016-04-12 15:50:16 -0400

    finished assignment

--- sketch.js
+++ sketch.js
@@ -1,27 +1,88 @@
 var cities;
 var city;
+var circleY = 0;
 
 function setup() {
 
   cities = createSelect();
   cities.option('New York,NY');
-  cities.option('Miami, FL');
+  cities.option('London,GB');
+  cities.option('Hong Kong,HK')
   //Change these cities and/or add your own
 
   cities.changed(loadCity);
   loadCity();
 
-  createCanvas(400,400);
+
+  createCanvas(900,600);
 
 }
 
 function draw() {
   if(city){ //Your drawing should go inside this if statement
+    background(city.main.temp,0,0);
+    fill("SkyBlue")
+    textSize(10);
+    text("black = cold",10,20);
+    text("red = hot",10,30)
 
-    background("grey");
-    text(city.name,50,50); //feel free to delete this
+    fill("white")
+    textSize(30);
+    text("humidity",5,70);
+    text("pressure",5,170)
+
+    stroke("white");
+    strokeWeight(10);
+    line(0,90,(city.main.humidity*10),90);
+    line(0,190,(city.main.pressure/2),190);
+
+//cloud drawing + number of clouds
+    ellipse(70,290,80,80);
+    ellipse(130,290,110,110);
+    ellipse(190,290,80,80);
+
+    textAlign(CENTER);
+    textSize(80);
+    fill("Cadetblue");
+    text(city.clouds.all,130,320);
+
+//wind speed
+    textSize(30);
+    text("wind speed",100,450);
+    text(city.wind.speed,230,450);
+    strokeWeight(4);
+    stroke(0,0,city.wind.speed*12);
+    line(200,400,500,400);
+    line(180,410,600,410);
+    line(270,420,620,420);
+    line(270,430,640,430);
+    line(270,440,650,440);
+    line(270,450,640,450);
+    line(270,460,620,460);
+    line(180,470,600,470);
+    line(200,480,500,480);
+
+//sunset
+    stroke("orange")
+    fill("yellow");
+    ellipse(800,circleY,100,100);
+    circleY = circleY + 1;
+    if(circleY >= windowWidth){
+      circleY = 0
+    }
+
+    strokeWeight(0);
+    fill("pink");
+    textSize(20);
+    text("time until sunset",780,300);
+    text(((city.sys.sunset*1000)-Date.now()),780,340);
 
 
+
+
+
+
+//city.main.humidity
   }
 }
 
